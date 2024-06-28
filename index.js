6cf4bf6b79e03f795fd5e1b7d90cfa715f948bd5
satvik5678 <satvikbhanwal5678@gmail.com>
2024-06-28 07:09:55 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//using static files
+//static files -> images, audios, videos, vanilla js, css
+//using static we can look them into a folder named public
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  fs.readdir(`./files`, function (err, files) {
+    res.render("index", { files: files });
+  });
+});
+
+app.get("/files/:filename", function (req, res) {
+  fs.readFile(
+    `./files/${req.params.filename}`,
+    "utf-8",
+    function (err, filedata) {
+      res.render("show", {
+        filename: req.params.filename,
+        filedata: filedata,
+      });
+    }
+  );
+});
+
+app.post("/create", function (req, res) {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.details,
+    function (err) {
+      res.redirect("/");
+    }
+  );
+});
+
+app.listen(4000);
